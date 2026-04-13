b774c297a21a74862173b97a71657652d393ed03
loangelo47 <lorenzobottecchia@gmail.com>
2026-04-13 22:01:55 +0100

    set up github

---
+++ Assets/_Project/Runtime/Player.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private PlayerCharacter playerCharacter;
+    [SerializeField] private PlayerCamera playerCamera; 
+
+    private PlayerInputsActions _inputActions;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        _inputActions = new PlayerInputsActions();
+        _inputActions.Enable();
+
+        playerCharacter.Initialize();
+        playerCamera.Initialize(playerCharacter.GetCameraTarget());
+    }
+
+    void OnDestroy()
+    {
+        _inputActions.Dispose();
+    }
+
+    void Update()
+    {
+        var input = _inputActions.Gameplay;
+        var deltaTime = Time.deltaTime;
+
+        // get camera input and update its rotation
+        var cameraInput = new CameraInput { Look = input.Look.ReadValue<Vector2>()};
+        playerCamera.UpdateRotation(cameraInput);
+
+        // Get character input and update it
+        var characterInput = new CharacterInput
+        {
+            Rotation    = playerCamera.transform.rotation,
+            Move        = input.Move.ReadValue<Vector2>(),
+            Jump        = input.Jump.WasPressedThisFrame(),
+            JumpSustain = input.Jump.IsPressed(),
+            Crouch      = input.Crouch.WasPressedThisFrame()
+                ? CrouchInput.Toggle
+                : CrouchInput.None
+        };
+        playerCharacter.UpdateInput(characterInput);
+        playerCharacter.UpdateBody(deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        playerCamera.UpdatePosition(playerCharacter.GetCameraTarget());
+    }
+}
---
+++ Assets/_Project/Runtime/PlayerCharacter.cs
@@ -0,0 +1,344 @@
+using UnityEngine;
+using KinematicCharacterController;
+
+public enum CrouchInput
+{
+    None, Toggle
+}
+
+public enum Stance
+{
+    Stand, Crouch, Slide
+}
+
+public struct CharacterState
+{
+    public bool Grounded;
+    public Stance Stance;
+}
+
+public struct CharacterInput
+{
+    public Quaternion Rotation;
+    public Vector2 Move;
+    public bool Jump;
+    public bool JumpSustain;
+    public CrouchInput Crouch;
+}
+
+public class PlayerCharacter : MonoBehaviour, ICharacterController
+{
+    [SerializeField] private KinematicCharacterMotor motor;
+    [SerializeField] private Transform root;
+    [SerializeField] private Transform cameraTarget;
+    [Space]
+    [SerializeField] private float walkSpeed = 20f;
+    [SerializeField] private float crouchSpeed = 7f;
+    [SerializeField] private float walkResponse = 25f;
+    [SerializeField] private float crouchResponse = 20f;
+    [Space]
+    [SerializeField] private float airSpeed = 15f;
+    [SerializeField] private float airAcceleration = 70f;
+    [Space]
+    [SerializeField] private float jumpSpeed = 20f;
+    [Range(0, 1f)]
+    [SerializeField] private float jumpSustainGravity = 0.4f;
+    [SerializeField] private float gravity = -90f;
+    [Space]
+    [SerializeField] private float slideStartSpeed = 25f;
+    [SerializeField] private float slideEndSpeed = 5f;
+    [SerializeField] private float slideFriction = 8f;
+    [Space]
+    [SerializeField] private float standHeight = 2f;
+    [SerializeField] private float crouchHeight = 1f;
+    [SerializeField] private float crouchHeightResponse = 15f;
+    [Range(0f, 1f)]
+    [SerializeField] private float standCameraTargetHeight = 0.9f;
+    [Range(0f, 1f)]
+    [SerializeField] private float crouchCameraTargetHeight = 0.7f;
+
+    private Stance _stance;
+
+    private CharacterState _state;
+    private CharacterState _lastState;
+    private CharacterState _tempState;
+
+    private Quaternion _requestedRotation;
+    private Vector3 _requestedMovement;
+    private bool _requestedJump;
+    private bool _requestedSustainedJump;
+    private bool _requestedCrouch;
+
+    private Collider[] _uncrouchOverlapResults;
+
+    public void Initialize()
+    {
+        _state.Stance = Stance.Stand;
+        _lastState = _state;
+
+        _uncrouchOverlapResults = new Collider[8];
+
+        motor.CharacterController = this;
+    }
+
+    public void UpdateInput(CharacterInput input)
+    {
+        _requestedRotation = input.Rotation;
+        // Take the 2d input vector and create a 3D movement vector on the XZ plane.
+        _requestedMovement = new Vector3(input.Move.x, 0f, input.Move.y);
+        // Clamp the length to 1 to prevent moving faster diagonally with WASD movement.
+        _requestedMovement = Vector3.ClampMagnitude(_requestedMovement, 1f);
+        // Orient the input so it's relative to the direction the player is facing
+        _requestedMovement = input.Rotation * _requestedMovement;
+
+        _requestedJump = _requestedJump || input.Jump;
+        _requestedSustainedJump = input.JumpSustain;
+        _requestedCrouch = input.Crouch switch
+        {
+          CrouchInput.Toggle => !_requestedCrouch,
+          _ => _requestedCrouch
+        };
+    }
+
+    public void UpdateBody(float deltaTime)
+    {
+        var currentHeight = motor.Capsule.height;
+        var normalizedHeight = currentHeight / standHeight;
+
+        var cameraTargetHeight = currentHeight *
+        (
+            _state.Stance is Stance.Stand
+                ? standCameraTargetHeight
+                : crouchCameraTargetHeight
+        );
+        var rootTargetScale = new Vector3(1f, normalizedHeight, 1f);
+
+        cameraTarget.localPosition = Vector3.Lerp
+        (
+            a: cameraTarget.localPosition,
+            b: new Vector3(0f, cameraTargetHeight, 0f),
+            t: 1f- Mathf.Exp(-crouchHeightResponse * deltaTime)
+        );
+        root.localScale = Vector3.Lerp
+        (
+            a: root.localScale, 
+            b: rootTargetScale,
+            t: 1f - Mathf.Exp(-crouchHeightResponse * deltaTime)
+        );
+    }
+
+    public void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime)
+    {
+        if (motor.GroundingStatus.IsStableOnGround)
+        
+        {
+            // If on the ground...
+            // Snap the requested movement direction to the angle of the surface
+            // the character is currently walking on.
+            
+            var groundedMovement = motor.GetDirectionTangentToSurface
+            (
+                direction: _requestedMovement,
+                surfaceNormal: motor.GroundingStatus.GroundNormal
+            ) * _requestedMovement.magnitude;
+
+            // Start Sliding
+            {
+                var moving = groundedMovement.sqrMagnitude > 0f;
+                var crouching = _state.Stance is Stance.Crouch;
+                var wasStanding = _lastState.Stance is Stance.Stand;
+                var wasInAir = !_lastState.Grounded;
+                if (moving && crouching && (wasStanding || wasInAir))
+                {
+                    _state.Stance = Stance.Slide;
+
+                    var slideSpeed = Mathf.Max(slideStartSpeed, currentVelocity.magnitude);
+                    currentVelocity = motor.GetDirectionTangentToSurface
+                    (
+                        direction : currentVelocity,
+                        surfaceNormal : motor.GroundingStatus.GroundNormal
+                    ) * slideSpeed;
+                }
+            }
+            // Move
+            if (_state.Stance is Stance.Stand or Stance.Crouch)
+            {
+                
+
+                // Calculate the speed and responsiveness of movement based
+                // on the character's stance
+                var speed = _state.Stance is Stance.Stand
+                    ? walkSpeed
+                    : crouchSpeed;
+                var response = _state.Stance is Stance.Stand
+                    ? walkResponse
+                    : crouchResponse;
+                    
+                // and move along the ground direction.
+                var targetVelocity = groundedMovement * speed;
+                currentVelocity = Vector3.Lerp
+                (
+                    a: currentVelocity,
+                    b: targetVelocity,
+                    t: 1f - Mathf.Exp(-response * deltaTime)
+                );
+            }
+            else
+            {
+                // Friction.
+                currentVelocity -= currentVelocity * (slideFriction * deltaTime);
+
+                // Stop.
+                if (currentVelocity.magnitude < slideEndSpeed)
+                    _state.Stance = Stance.Crouch;
+            }
+        }
+        // else in the air...
+        else
+        {
+            // Move.
+            if (_requestedMovement.sqrMagnitude > 0f)
+            {
+                // Requested movement projected onto movement plane. (magnitude preserved)
+                var planarMovement = Vector3.ProjectOnPlane
+                (
+                    vector: _requestedMovement,
+                    planeNormal: motor.CharacterUp
+                ) * _requestedMovement.magnitude;
+
+                // current velocity on movement plane
+                var currentPlanarVelocity = Vector3.ProjectOnPlane
+                (
+                    vector: currentVelocity,
+                    planeNormal: motor.CharacterUp
+                );
+
+                // Calculate movement force
+                var movementForce = planarMovement * airAcceleration * deltaTime;
+
+                // Add it to the planar velocity for a target velocity.
+                var targetPlanarVelocity = currentPlanarVelocity + movementForce;
+
+                // Limit target velocity to air speed.
+                targetPlanarVelocity = Vector3.ClampMagnitude(targetPlanarVelocity, airSpeed);
+
+                // Steer towards currrent velocity.
+                currentVelocity += targetPlanarVelocity - currentPlanarVelocity;
+            }
+            // Gravity.
+            var effectiveGravity = gravity;
+            var verticalSpeed = Vector3.Dot(currentVelocity, motor.CharacterUp);
+            if (_requestedSustainedJump && verticalSpeed > 0f)
+                effectiveGravity *= jumpSustainGravity;
+
+            currentVelocity += motor.CharacterUp * effectiveGravity * deltaTime;
+        }
+
+        if (_requestedJump)
+        {
+            _requestedJump = false;     // Unset jump request.
+            _requestedCrouch = false;   // And request the character uncrouched.
+
+            // unstick the player from the ground.
+            motor.ForceUnground(time: 0.1f);
+
+            // Set minimum vertical speed to the jump speed.
+            var currentVerticalSpeed = Vector3.Dot(currentVelocity, motor.CharacterUp);
+            var targetVerticalSpeed = Mathf.Max(currentVerticalSpeed, jumpSpeed);
+            // Add the difference in current and target vertical speed to the character's velocity.
+            currentVelocity += motor.CharacterUp * (targetVerticalSpeed - currentVerticalSpeed);
+        }
+    }
+    public void UpdateRotation(ref Quaternion currentRotation, float deltaTime)
+    {
+        // update the character's rotation to face in the same direction as the
+        // resquested rotation (camera rotation)
+
+        // I don't want the character to rotate up and down, so the direction of the character
+        // looks should only be locked to X axis / 'flatttened' in a sense
+
+        //this is done by projecting a vector pointing in the same direction that
+        //the player is looking onto a flat ground plane
+
+        var forward = Vector3.ProjectOnPlane
+        (
+            _requestedRotation * Vector3.forward,
+            motor.CharacterUp
+        );
+
+        if (forward.sqrMagnitude > 0f)
+        {
+            currentRotation = Quaternion.LookRotation(forward, motor.CharacterUp);
+        }
+    }
+
+    public void BeforeCharacterUpdate(float deltaTime)
+    {
+        _tempState = _state;
+
+        // Crouch.
+        if (_requestedCrouch && _state.Stance == Stance.Stand)
+        {
+            _state.Stance = Stance.Crouch;
+            motor.SetCapsuleDimensions
+            (
+                radius: motor.Capsule.radius,
+                height: crouchHeight,
+                yOffset: crouchHeight * 0.5f
+            );
+        }
+    }
+    public void PostGroundingUpdate(float deltatime)
+    {
+        if (!motor.GroundingStatus.IsStableOnGround && _state.Stance is Stance.Slide)
+            _state.Stance = Stance.Crouch;
+    }
+    public void AfterCharacterUpdate(float deltaTime)
+    {
+        // Uncrouch.
+        if (!_requestedCrouch && _state.Stance is not Stance.Stand)
+        {
+            // hesitantly 'standup' the character by checking if there's enough space above the character to fit the stand height.
+            motor.SetCapsuleDimensions
+            (
+                radius: motor.Capsule.radius,
+                height: standHeight,
+                yOffset: standHeight * 0.5f
+            );
+
+            // check if capsule overlaps with any colliders before uncrouching.
+            var pos = motor.TransientPosition;
+            var rot = motor.TransientRotation;
+            var mask = motor.CollidableLayers;
+            if (motor.CharacterOverlap(pos, rot, _uncrouchOverlapResults, mask, QueryTriggerInteraction.Ignore) > 0)
+            {
+                // Re-crouch
+                _requestedCrouch = true;
+                motor.SetCapsuleDimensions
+                (
+                    radius : motor.Capsule.radius,
+                    height : standHeight,
+                    yOffset: standHeight * 0.5f
+                );
+            }
+            else
+            {
+                _state.Stance = Stance.Stand;
+            }
+        }
+
+        // Update state to reflect relevant motor properties.
+        _state.Grounded = motor.GroundingStatus.IsStableOnGround;
+        // And update the _lastState to store the character state snapshot taken at
+        // the beggining of this character update
+        _lastState = _tempState;
+    }
+
+    public void OnGroundHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport){}
+    public void OnMovementHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport){}
+    public bool IsColliderValidForCollisions(Collider coll) => true;
+    public void OnDiscreteCollisionDetected(Collider hitCollider){}
+    public void ProcessHitStabilityReport(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, Vector3 aCharacterPosition, Quaternion atCharacterRotation, ref HitStabilityReport hitStabilityReport){} 
+
+    public Transform GetCameraTarget() => cameraTarget;
+}
